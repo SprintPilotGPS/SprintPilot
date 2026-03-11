cd44b0a6a56961868006c18486b87e5f00d683f0
Naivc <congchanghong@gmail.com>
2026-03-11 11:22:20 +0100

    inicio de config

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const viewRoutes = require('./routes/views');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../public/views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/', viewRoutes);
+app.use('/api', apiRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+module.exports = app;
