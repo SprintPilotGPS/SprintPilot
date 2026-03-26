39bb32a683dc8bbb4c14f0f0d6d9c718f874b546
JorgeGUCM <jogome22@ucm.es>
2026-03-26 18:29:03 +0100

    Terminado mostrar respuestas recibidas del servidor

--- public/js/projectValidation.js
+++ public/js/projectValidation.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   const modal = document.getElementById("projectModal");
 
   $("#openModal").on("click", function () {
-    modal.showModal(); // Esto hará que aparezca con el fondo oscuro
+    modal.classList.add("show"); // Esto hará que aparezca con el fondo oscuro
   });
   // Cerrar el modal
   $("#closeModal").on("click", function () {
-    modal.close();
+    modal.classList.remove("show");
     $("#projectForm")[0].reset();
   });
 
@@ -20,19 +20,48 @@ $(document).ready(function () {
       descripcion: $("#descripcion").val().trim(), // Enviará "" si está vacío
     };
 
+    function showResponse(res) {
+      let noti = document.querySelector("#noti");
+      let cards = document.querySelector("#projects-cards");
+
+      if (res.success) {
+        noti.innerHTML = `<p class="badge bg-success fs-5">Proyecto creado correctamente</p>`;
+        
+        let descripcion = (res.data.descripcion == "")? "No hay descripción." : res.data.descripcion;
+        let card = `
+        <div class="p-4 card scale-card card-proj">
+            <div class="d-flex justify-content-between">
+              <h2 class="mb-1">` + res.data.nombre + `</h2>
+              <p class="fs-5 mb-1 align-self-center badge bg-primary">` + res.data.identificador + `</p>
+            </div>
+            <hr />
+            <p>` + descripcion + `</p>
+        </div>`
+        if(document.querySelector("#projects-cards > p") == null)
+          cards.innerHTML += card;
+        else
+          cards.innerHTML = card;
+
+        modal.classList.remove("show");
+      } else {
+        noti.innerHTML = `<p class="badge bg-warning fs-5">` + res.error + `</p>`;
+      }
+      noti.classList.add("show");
+      setTimeout(() => noti.classList.remove("show"), 5000);
+    }
+
     $.ajax({
       url: "/api/projects",
       type: "POST",
       contentType: "application/json",
       data: JSON.stringify(formData),
       success: function (response) {
-        alert("✅ Proyecto guardado correctamente");
-        modal.close();
-        location.reload(); // Recarga la página para ver los cambios
+        //modal.classList.remove("show");
+        showResponse(response);
       },
       error: function (xhr) {
         const msg = xhr.responseJSON ? xhr.responseJSON.error : "Error al guardar";
-        alert("❌ " + msg);
+        showResponse({success: false, error: msg});
       },
     });
   });
