b74c8eef45ccbbec8561fc6700e2b0222c1b3ba1
Monica Martin <92672253+moni2319@users.noreply.github.com>
2026-04-17 13:21:55 +0200

    Crear modelo base de datos para Sprints

---
+++ src/models/Sprint.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+// Definimos la estructura basada en la foto de la pizarra
+const sprintSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true // Al ser un ID, es buena idea que sea único
+  },
+  idProyecto: {
+    type: String,
+    required: true
+  },
+  fechaIni: {
+    type: Date,
+    required: true
+  },
+  fechaFin: {
+    type: Date,
+    required: true
+  },
+  HU: [{
+    type: Number // Es un array de números (los IDs de las Historias de Usuario)
+  }],
+  sprintGoal: {
+    type: String
+  }
+}, {
+  // Esto añade automáticamente createdAt y updatedAt (opcional pero muy recomendado)
+  timestamps: true 
+});
+
+// Creamos y exportamos el modelo
+const Sprint = mongoose.model("Sprint", sprintSchema);
+
+module.exports = Sprint;
